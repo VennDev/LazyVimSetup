f4bb5f6d91635b4a257a139b6d9381b15c90d4d8
VennV <111500380+VennDev@users.noreply.github.com>
2024-09-12 01:52:22 +0700

    Delete lua directory

--- lua/config/keymaps.lua
+++
@@ -1,9 +0,0 @@
--- Keymaps are automatically loaded on the VeryLazy event
--- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
-
-vim.keymap.set('i', '<C-J>', 'copilot#Accept("\\<CR>")', {
-  expr = true,
-  replace_keycodes = false
-})
-vim.g.copilot_no_tab_map = true
